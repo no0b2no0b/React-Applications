a0538d9422e0d73b9566f79468c54bf8fe8924aa
Ajay yadav <no0b2no0b>
2023-02-24 12:01:26 +0530

    my-react-app

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar title='TextUtils' about='About TextUtils'/> */}
+      {/* <Navbar/> */} 
+      <Navbar title = 'TextUtils'/>
+    </>
+  );
+}
+
+export default App;
